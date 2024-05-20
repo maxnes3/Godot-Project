fe9d6c7aa9bf45645eef52b0cec0e0c6996164ff
maxnes3 <112558334+maxnes3@users.noreply.github.com>
2024-05-21 02:44:59 +0400

    all bases connected

--- Scripts/TileMapController.cs
+++ Scripts/TileMapController.cs
@@ -4,35 +4,37 @@ using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 public partial class TileMapController : TileMap
 {
 
-	private readonly int mapGridSize = 251; //The size of map (X * X), where X % 2 != 0
-	private int[,] matrixMap; //The matrix of map index
-	private readonly int centerCount = 6; //Count of zone center
+	private readonly int mapGridSize = 251; // The size of map (X * X), where X % 2 != 0
+	private int[,] matrixMap; // The matrix of map index
+	private readonly int centerCount = 6; // Count of zone center
 
-	//Action after spawn this object
+	// Action after spawn this object
 	public override void _Ready()
 	{
-		//Generate map on matrix
+		// Generate map on matrix
 		matrixMap = GenerateMapMatrix();
 
-		//Draw tiles on map
+		// Draw tiles on map
 		DrawTileCells(matrixMap, 0);
 
-		//Change TileMap position 
+		// Change TileMap position 
 		Position -= new Vector2I(TileSet.TileSize.X * (mapGridSize - 1) / 2 + TileSet.TileSize.X / 2,
 			TileSet.TileSize.Y * (mapGridSize - 1) / 2 + TileSet.TileSize.Y / 2);
 	}
 
-	//Furction return complete map
+	// Furction return complete map
 	private int[,] GenerateMapMatrix()
 	{
 		Random rnd = new Random();
 		int[,] newMatrixMap = new int[mapGridSize, mapGridSize];
-		int[,] barrierMap = new int[mapGridSize, mapGridSize];
+		int[,] barrierMap;
 		int barriersPercent = 40; //Percent from 0 to 100
+		int pathPercentWidth = 33;
 		int baseRadius = rnd.Next(mapGridSize / 15, mapGridSize / 13);
 		int borderOffest = rnd.Next(baseRadius + 1, mapGridSize / 11);
 
@@ -52,19 +54,33 @@ public partial class TileMapController : TileMap
 		// Function to place a base
 		void FillBlock(Vector2I center)
 		{
-			for (int dx = -baseRadius; dx <= baseRadius; ++dx)
+			// Generate random radii for the ellipse
+			int radiusX = rnd.Next(mapGridSize / 15, mapGridSize / 13);
+			int radiusY = rnd.Next(mapGridSize / 15, mapGridSize / 13);
+
+			for (int dx = -radiusX; dx <= radiusX; ++dx)
 			{
-				for (int dy = -baseRadius; dy <= baseRadius; ++dy)
+				for (int dy = -radiusY; dy <= radiusY; ++dy)
 				{
 					int x = center.X + dx;
 					int y = center.Y + dy;
-					newMatrixMap[x, y] = 1;
+					if (x >= 0 && x < mapGridSize && y >= 0 && y < mapGridSize)
+					{
+						// Check if the point is within the ellipse
+						if ((dx * dx) * (radiusY * radiusY) + (dy * dy) * (radiusX * radiusX) <= (radiusX * radiusX) * (radiusY * radiusY))
+						{
+							newMatrixMap[x, y] = 1;
+						}
+					}
 				}
 			}
 		}
 
+		// Generate random barriers cell on the barrierMap
 		void GenerateRandomBarrierMap(int count)
 		{
+			barrierMap = new int[mapGridSize, mapGridSize];
+			
 			int rows = barrierMap.GetLength(0);
 			int cols = barrierMap.GetLength(1);
 
@@ -93,8 +109,56 @@ public partial class TileMapController : TileMap
 			}
 		}
 
+		bool CreateBasePaths()
+		{
+			HashSet<int> connectedBases = new HashSet<int>();
+
+			for (int i = 0; i < baseCenters.Count; ++i)
+			{
+				int startBase, endBase;
+				do
+				{
+					startBase = rnd.Next(0, baseCenters.Count);
+					endBase = rnd.Next(0, baseCenters.Count);
+				} while (startBase.Equals(endBase));
+
+				connectedBases.Add(startBase);
+				connectedBases.Add(endBase);
+
+				if (!CreateBFSPath(baseCenters[startBase], baseCenters[endBase]))
+				{
+					newMatrixMap = new int[mapGridSize, mapGridSize];
+					return false;
+				}
+			}
+
+			for (int i = 0; i < baseCenters.Count; ++i)
+			{
+				if (!connectedBases.Contains(i))
+				{
+					int secondBase;
+
+					do
+					{
+						secondBase = rnd.Next(0, baseCenters.Count);
+					} while (i.Equals(secondBase));
+
+					connectedBases.Add(i);
+					connectedBases.Add(secondBase);
+
+					if (!CreateBFSPath(baseCenters[i], baseCenters[secondBase]))
+					{
+						newMatrixMap = new int[mapGridSize, mapGridSize];
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
 		// Function to create a path between two points
-		void CreateBFSPath(Vector2I start, Vector2I end)
+		bool CreateBFSPath(Vector2I start, Vector2I end)
 		{
 			List<(int, int)> shortestlPath = FindShortestPath((start.X, start.Y), (end.X, end.Y));
 			if (shortestlPath != null)
@@ -103,13 +167,16 @@ public partial class TileMapController : TileMap
 				{
 					newMatrixMap[cell.Item1, cell.Item2] = 1;
 				}
+				return true;
 			}
+			return false;
 		}
 
 		List<(int, int)> FindShortestPath((int x, int y) start, (int x, int y) end)
 		{
 			int rows = barrierMap.GetLength(0);
 			int cols = barrierMap.GetLength(1);
+
 			var directions = new (int, int)[]
 			{
 				(0, 1), (1, 0), (0, -1), (-1, 0),  // right, down, left, up
@@ -146,32 +213,22 @@ public partial class TileMapController : TileMap
 			return null;
 		}
 
-		GenerateRandomBarrierMap(mapGridSize * mapGridSize * barriersPercent / 100);
-
-		for (int i = 0; i < baseCenters.Count; ++i)
+		// Create paths between two bases on the map
+		do
 		{
-			int start = rnd.Next(0, baseCenters.Count);
-			int end = rnd.Next(0, baseCenters.Count);
-
-			while (start.Equals(end))
-			{
-				start = rnd.Next(0, baseCenters.Count);
-				end = rnd.Next(0, baseCenters.Count);
-			}
-
-			CreateBFSPath(baseCenters[start], baseCenters[end]);
-		}
+			GenerateRandomBarrierMap(mapGridSize * mapGridSize * barriersPercent / 100);
+		} while (!CreateBasePaths());
 
 		// Place all bases on the map
 		foreach (Vector2I baseCenter in baseCenters)
 		{
-            FillBlock(baseCenter);
+			FillBlock(baseCenter);
 		}
 
 		return newMatrixMap;
 	}
 
-	//Draw Matrix Tiles on TileMap
+	// Draw Matrix Tiles on TileMap
 	private void DrawTileCells(int[,] matrix, int layer)
 	{
 		for (int x = 0; x < mapGridSize; ++x)
